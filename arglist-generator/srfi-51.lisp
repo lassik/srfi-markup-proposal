3d0d1bbea857893a7aa8343d9baef7402dbd40a7
Lassi Kortela <lassi@lassi.io>
2019-03-07 16:14:04 +0200

    Add SRFI 51 (partially done)

---
+++ arglist-generator/srfi-51.lisp
@@ -0,0 +1,56 @@
+
+(syntax arg-and
+  (arg caller optional)
+  (arg variable)
+  (arg expr)
+  (arg ... rest))
+
+(syntax arg-ands
+  (quoted-symbol common optional)
+  (arg caller optional)
+  (sublist
+    (arg caller optional)
+    (arg variable)
+    (arg expr)
+    (arg ... rest))
+  (arg ... rest))
+
+(syntax err-and
+  (arg caller)
+  (arg expression)
+  (arg ... rest))
+
+(syntax err-ands
+  (sublist
+    (arg caller)
+    (arg expression)
+    (arg ... rest))
+  (arg ... rest))
+
+(syntax arg-or
+  (arg caller optional)
+  (arg variable)
+  (arg expr)
+  (arg ... rest))
+
+(syntax arg-ors
+  (quoted-symbol common optional)
+  (arg caller optional)
+  (sublist
+    (arg caller optional)
+    (arg variable)
+    (arg expr)
+    (arg ... rest))
+  (arg ... rest))
+
+(syntax err-or
+  (arg caller)
+  (arg expression)
+  (arg ... rest))
+
+(syntax err-ors
+  (sublist
+    (arg caller)
+    (arg expression)
+    (arg ... rest))
+  (arg ... rest))
